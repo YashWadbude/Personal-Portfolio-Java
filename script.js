90a811b74e1376b3fac8ce7e6bacdcbbda15f716
YashWadbude <154418117+YashWadbude@users.noreply.github.com>
2026-07-18 01:04:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ const projects = [
   {
     title: 'Movie',
     img: 'movie.jpeg',
-    link: 'https://nocostnights.onrender.com/' // replace with actual link
+    link: 'https://nocostnights.onrender.com' // replace with actual link
   }
 ];
 
